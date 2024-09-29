0c34eb44e7653ff278b72810e9aa7b80db341a2e
Mangesh <157815072+MangeshBhalerao@users.noreply.github.com>
2024-09-29 11:26:26 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,10 +3,15 @@ import { Search } from 'lucide-react'
 
 function HeroSection() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-r from-gray-800 to-gray-900">
+    <section
+      className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-cover bg-center"
+      style={{
+        backgroundImage: 'url(https://64.media.tumblr.com/1972a18bebff1d80af4e8f25f3325f16/tumblr_o6tkwjAGsQ1smqdb1o1_400.gifv)',
+      }}
+    >
       <div className="container px-4 md:px-6 mx-auto">
         <div className="flex flex-col items-center space-y-4 text-center">
-          <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-blue-400">
+          <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-[#CBE4DE]">
             Navigate Your Future
           </h1>
           <p className="mx-auto max-w-[700px] text-gray-400 md:text-xl">
@@ -21,7 +26,7 @@ function HeroSection() {
               />
               <button
                 type="submit"
-                className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none bg-blue-600 text-white hover:bg-blue-700 h-10 py-2 px-4"
+                className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none bg-[#0E8388] text-white hover:bg-[#2E4F4F] h-10 py-2 px-4"
               >
                 <Search className="h-4 w-4" />
                 <span className="sr-only">Search</span>
@@ -30,7 +35,8 @@ function HeroSection() {
           </div>
         </div>
       </div>
-    </section>
+  </section>
+  
   )
 }
 
